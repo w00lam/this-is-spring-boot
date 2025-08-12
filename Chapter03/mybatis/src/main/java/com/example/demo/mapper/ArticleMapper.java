972a17ee689e7f71b0bdb2ab972078c829fbc2f7
Woolam <wlam135@naver.com>
2025-08-12 23:25:48 +0900

    mybatis

--- Chapter03/mybatis/src/main/java/com/example/demo/mapper/ArticleMapper.java
+++ Chapter03/mybatis/src/main/java/com/example/demo/mapper/ArticleMapper.java
@@ -4,20 +4,28 @@ import com.example.demo.model.Article;
 import org.apache.ibatis.annotations.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface ArticleMapper {
+    @Results(id = "articleResult", value = {
+            @Result(property = "title", column = "title"),
+            @Result(property = "description", column = "content"),
+            @Result(property = "memberId", column = "member_id")
+    })
     @Select("SELECT * FROM article")
     List<Article> selectAll();
 
-    @Select("SELECT COUNT(*) FROM article")
-    int selectAllCount();
-
-    @Select("SELECT * FROM article WHERE id = #{id}")
-    Article selectById(@Param("id") Long id);
+    @ResultMap("articleResult")
+    @Select("SELECT * FROM article WHERE id=#{id}")
+    Optional<Article> selectById(@Param("id") Long id);
 
     @Select("SELECT * FROM article WHERE member_id=#{member.id}")
+    @ResultMap("articleResult")
     List<Article> selectByMemberId(@Param("memberId") Long memberId);
+
+    @Select("SELECT COUNT(*) FROM article")
+    int selectAllCount();
 
     @Insert("""
             INSERT INTO article(title, description, created, updated, member_id)
