2884d8854690e1dcc53f68ed7d959229f45f51a8
Woolam <wlam135@naver.com>
2025-10-21 10:02:40 +0900

    study up to 616

--- Chapter08/rag/src/test/java/com/example/demo/OpenAiEmbeddingModelTests.java
+++ Chapter08/rag/src/test/java/com/example/demo/OpenAiEmbeddingModelTests.java
@@ -3,15 +3,23 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.document.Document;
+import org.springframework.ai.document.DocumentReader;
 import org.springframework.ai.embedding.Embedding;
 import org.springframework.ai.embedding.EmbeddingResponse;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.OpenAiEmbeddingModel;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.List;
 
 @SpringBootTest
@@ -55,5 +63,49 @@ public class OpenAiEmbeddingModelTests {
         String text2 = "두 번째 텍스트 문서입니다.";
         List<Document> documents = List.of(new Document(text1), new Document(text2));
         vectorStore.write(documents);
+    }
+
+    @Test
+    public void textReader() {
+        DocumentReader reader = new TextReader("classpath:/운수좋은날.txt");
+        List<Document> documents = reader.read();
+
+        TokenTextSplitter splitter = new TokenTextSplitter();
+        List<Document> chunks = splitter.split(documents);
+
+        vectorStore.write(splitter.split(chunks));
+    }
+
+    @Test
+    public void pdfReader() throws IOException {
+        var reader = new PagePdfDocumentReader("classpath:/인공지능_시대의_예술.pdf");
+        List<Document> documents = reader.read();
+
+        TokenTextSplitter splitter = new TokenTextSplitter();
+        List<Document> chunks = splitter.split(documents);
+
+        vectorStore.write(splitter.split(chunks));
+    }
+
+    @Test
+    public void textReaderSimilaritySearch() {
+        String question = "김첨지 아내는 무슨 병에 걸렸나요?";
+        var documents = vectorStore.similaritySearch(question);
+
+        assert documents != null;
+        var information = String.join("\n", documents.stream().map(Document::getText).toList());
+
+        String prompt = MessageFormat.format("""
+                다음의 정보를 기반으로 하여 답을 하고,
+                정보가 없는 경우에는 모른다고 답변하세요.
+                [정보]
+                {0}
+                [질문]
+                {1}
+                """, information, question);
+
+        Message message = new UserMessage(prompt);
+        String result = chatModel.call(message);
+        log.info("result = {}", result);
     }
 }
